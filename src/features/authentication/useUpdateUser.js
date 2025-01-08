61eb5d72524630d38179d8b1cf2d3bdfbd627bfe
Harsh <harshjha8453@gmail.com>
2025-01-09 02:07:03 +0530

    feat: add Header component with user account features
    - Added Header component with logout and account section
    - Displayed user name and avatar in the header
    - Implemented functionality for users to upload an avatar, change full name, and update password

---
+++ src/features/authentication/useUpdateUser.js
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateCurrentUser } from "../../services/apiAuth";
+
+export const useUpdateUser = () => {
+    const queryClient = useQueryClient();
+    const { mutate:updateUser, isLoading: isUpdating } = useMutation({
+        mutationFn: updateCurrentUser,
+        onSuccess: (user) => {
+          toast.success("user successfully updated");
+        //   queryClient.setQueryData('user',user)
+          queryClient.invalidateQueries({ queryKey: ["user"] });
+        },
+        onError: (err) => toast.error(err.message),
+      });
+
+      console.log(updateUser)
+      return {updateUser,isUpdating}
+
+}
